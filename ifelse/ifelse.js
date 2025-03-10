baba5a8752b04c133dc535b99a20ef7fbf9c72d1
Anish <anishvaghasiya001@gmail.com>
2025-03-10 15:59:48 +0530

    ifelse

---
+++ ifelse/ifelse.js
@@ -0,0 +1,18 @@
+let a=20;
+let b=40;
+
+if(a>b){
+    console.log("a is greater than b");
+}
+else{
+    console.log("b is greater than a");
+}
+
+let age=56;
+
+if(age>=18){
+    console.log("you allowed for voting");
+}
+else{
+    console.log("you are not allowed for voting");
+}
