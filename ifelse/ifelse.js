921226e58f6f1151aa83a73943dcfbd061b52e3e
Anish <anishvaghasiya001@gmail.com>
2025-03-11 15:27:49 +0530

    elseif

--- ifelse/ifelse.js
+++ ifelse/ifelse.js
@@ -1,18 +1,69 @@
-let a=20;
-let b=40;
+// let a=20;
+// let b=40;
 
-if(a>b){
-    console.log("a is greater than b");
-}
-else{
-    console.log("b is greater than a");
-}
+// if(a>b){
+//     console.log("a is greater than b");
+// }
+// else{
+//     console.log("b is greater than a");
+// }
 
-let age=56;
+// let age=56;
 
-if(age>=18){
-    console.log("you allowed for voting");
-}
-else{
-    console.log("you are not allowed for voting");
-}
+// if(age>=18){
+//     console.log("you allowed for voting");
+// }
+// else{
+//     console.log("you are not allowed for voting");
+// }
+
+//wpa a find largest number from a , b , c;
+
+let a=20 , b=30 , c=15;
+
+if(b>c){
+    if(b>a){
+        console.log("b is large number");
+    }else{
+        console.log("a is large number");
+    }}else{
+        if(a>c){
+            console.log("a is greatest number");
+        }
+        else{
+            console.log("c is greatest number");
+        }
+    }
+
+    let x=10 , y=20 , z=40;
+
+    if(x>y&&x>z){
+        console.log("x is greater number");
+    }
+    else if(y>z){
+        console.log("y is greater number");
+    }
+    else {
+        console.log("z is greater number");
+    }
+
+    let p=20 , q=10 , r=40;
+
+    if(r>p){
+        if(r>q){
+            console.log("r is large number");
+        }else{
+            console.log("q is large number");
+        }
+    }else{
+        if(p>q){
+            console.log("p is greater number");
+        }else{
+            console.log("q is large number");
+        }
+    }
+
+
+
+
+
