8289a6abe91412fc7d7ab5820eba72cfdda8fbc9
vaghasiyanaish <anishvaghasiya001@gmail.com>
2025-03-19 15:26:49 +0530

    LOOP1

---
+++ loop/loop.js
@@ -0,0 +1,27 @@
+//for loop
+
+for (let i=1;i<=10;i++){
+    console.log(i);
+}
+
+for(let a=1;a<=20;a++){
+    console.log(a+".anish vaghasiya")
+}
+
+//while loop
+
+let p=20;
+
+while(p>=5){
+    console.log(p);
+    p--;
+}
+
+//do while loop
+
+let b=30
+
+do{
+    console.log(b);
+    b--;
+}while(b>=10);
